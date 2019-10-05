87e3e4a1809b40df2c0b5abf1d2e7c3587de8585
Maqarios <maqarioswagdy@gmail.com>
2019-10-05 22:40:51 +0200

    feat: add PrintGame Function

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
+
+import cells.*;
+
 import java.awt.Point;
 import java.util.ArrayList;
 
 import endgame.EndGameProblem;
+import endgame.EndGameState;
 
 public class Main {
 
-	public static void parse() {
+	public static EndGameProblem parse() {
 		Scanner sc = new Scanner(System.in);
 
 		// Reading Line
 		String line = sc.nextLine();
+		sc.close();
+		
 		String[] lineElements = line.split(";");
 
 		// Constructing Map
@@ -37,14 +43,43 @@ public class Main {
 		for (int i = 0; i < elements.length; i += 2)
 			warriorsLoc.add(new Point(Integer.parseInt(elements[i]), Integer.parseInt(elements[i + 1])));
 
-		new EndGameProblem(ironManLoc, thanosLoc, stonesLoc, warriorsLoc, mapDimensions);
-
-		sc.close();
+		
+		
+		return new EndGameProblem(ironManLoc, thanosLoc, stonesLoc, warriorsLoc, mapDimensions);
+	}
+	
+	public static void PrintEndGame(EndGameProblem game) {
+		Cell[][] map = game.constructMap(((EndGameState) game.getInitialState()));
+		
+		for(int i = 0; i < (map.length * 2); i++)
+			System.out.print("- ");
+		System.out.println();
+		
+		for(int x = 0; x < map.length; x++) {
+			for(int y = 0; y < map[x].length; y++) {
+				if(map[x][y] instanceof EmptyCell)
+					System.out.print(" " + " | ");
+				if(map[x][y] instanceof IronManCell)
+					System.out.print("I" + " | ");
+				if(map[x][y] instanceof StoneCell)
+					System.out.print("S" + " | ");
+				if(map[x][y] instanceof ThanosCell)
+					System.out.print("T" + " | ");
+				if(map[x][y] instanceof WarriorCell)
+					System.out.print("W" + " | ");
+			}
+			System.out.println();
+			
+			for(int i = 0; i < (map.length * 2); i++)
+				System.out.print("- ");
+			
+			System.out.println();
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		parse();
+		EndGameProblem game = parse();
+		PrintEndGame(game);
 	}
 
 }
