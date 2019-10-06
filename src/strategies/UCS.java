482f13d07378eb0f8bf2a86f72b63c1af3d6476c
Logician724 <king2sairos2013@gmail.com>
2019-10-06 23:26:46 +0200

    fix: Handle issue on adding expanded nodes in UCS

--- src/strategies/UCS.java
+++ src/strategies/UCS.java
@@ -10,22 +10,22 @@ public class UCS extends SearchStrategy {
 
     @Override
     public ArrayList<Node> execute(ArrayList<Node> nodes, ArrayList<Node> expandedNodes) {
-    	Iterator<Node> iterator = expandedNodes.iterator();
-    	while(iterator.hasNext()) {
+        Iterator<Node> iterator = expandedNodes.iterator();
+        while (iterator.hasNext()) {
             Node expandedNode = iterator.next();
-    		if(nodes.isEmpty()) {
-    			nodes.add(expandedNode);
-    		}else {
-    			// If there are items in the nodes list insert the expanded nodes in order
-    			for (int i = 0; i < nodes.size(); i++) {
+            if (nodes.isEmpty()) {
+                nodes.add(expandedNode);
+            } else {
+                // If there are items in the nodes list insert the expanded nodes in order
+                for (int i = 0; i < nodes.size(); i++) {
                     if (nodes.get(i).compareTo(expandedNode) > 0) {
                         nodes.add(i, expandedNode);
                         break;
                     }
+                    nodes.add(expandedNode);
                 }
-    		}
+            }
         }
-    	System.out.println(nodes.size());
         return nodes;
     }
 
